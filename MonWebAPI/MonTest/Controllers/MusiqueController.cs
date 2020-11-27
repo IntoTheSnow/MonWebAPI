af32df8ba35e8e07fbd9aacb420cd7f4c16eb59e
Chuanbin <foreverqichuan@gmail.com>
2020-11-27 12:12:40 -0500

    del

---
+++ MonWebAPI/MonTest/Controllers/MusiqueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonTest.Controllers
+{
+    public class MusiqueController : Controller
+    {
+        // GET: Musique
+        public ActionResult MusiqueListe()
+        {
+            return View();
+        }
+    }
+}
