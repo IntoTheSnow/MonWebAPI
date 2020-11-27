22611906ab0faf0afda6da5ef3cd155a68d223ac
Chuanbin <foreverqichuan@gmail.com>
2020-11-27 15:22:32 -0500

    mvc-> jquery -> ajax -> endpoint api -> parseJson-> afficher info

--- MonWebAPI/MonMusiqueSite/Scripts/main.js
+++ MonWebAPI/MonMusiqueSite/Scripts/main.js
@@ -11,7 +11,11 @@
             data:{},
             success: function (result,status) {
                 if (status == "success") {
-                    alert(result);
+                    var musiqueListe = $.parseJSON(result).result;
+                    var chansonsParType = musiqueListe[0].content;  //以后可以设置变量
+                    $.each(chansonsParType, function (n, item) {  //.parseJSON（）方法把JSON字符串转换为javascript对象，不转换的话将会抛出错误。
+                        $("#musiqueListe").append("<p>演唱者: " + item.author + "</br>歌 名: " + item.title + "</p>");//控制输出的数据格式
+                    })
                 }
             },
             error: function (error) {
